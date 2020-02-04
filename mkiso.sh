7a293b6c391ca31f2dab9d7b163f116363e87d5a
Tasos Sahanidis <tasos@tasossah.com>
2020-02-04 11:57:22 +0200

    Modify mkiso script to decompress the dummy

--- mkiso.sh
+++ mkiso.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/bash
-mkisofs -f -V TEST -sysid PLAYSTATION -xa2 -o out.iso dist
+if [[ ! -f "dist/0.0" ]]; then
+    gunzip -k dist/0.0.gz
+fi
+mkisofs -f -V TEST -sysid PLAYSTATION -xa2 -o SPCMechanic.iso dist
